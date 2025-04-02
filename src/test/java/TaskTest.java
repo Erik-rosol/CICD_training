25c13e67bf008bc6e4212254536942fde5b964b2
Erik-rosol <erik.vila@rosol-technology.com>
2025-04-02 10:34:32 +0200

    testTaskCreation method has been make public

--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class TaskTest {
     @Test
-    void testTaskCreation(){
+    public void testTaskCreation(){
         Task task = new Task(1, "Write unit test");
         assertEquals(1, task.getId());
         assertEquals("Write unit test", task.getDescription());
